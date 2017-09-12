309b44585341a43e2ca394005beb9e4aabd1fb34
jason <jason@blend.com>
2017-09-11 23:45:23 -0700

    arimoost

--- src/graph.js
+++ src/graph.js
@@ -1,10 +1,10 @@
 
-var svg = d3.select("svg"),
+let svg = d3.select("svg"),
     margin = {top: 20, right: 20, bottom: 30, left: 50},
     width = svg.attr("width") - margin.left - margin.right,
     height = svg.attr("height") - margin.top - margin.bottom;
 
-var tooltip = d3.select("body")
+let tooltip = d3.select("body")
 	.append("div")
 	.style("position", "absolute")
 	.style("z-index", "10")
@@ -12,32 +12,32 @@ var tooltip = d3.select("body")
 	.style("background-color", "white")
 	.text("");
 
-var parseDate = d3.timeParse("%Y-%m-%d");
+let parseDate = d3.timeParse("%Y-%m-%d");
 
-var x = d3.scaleTime().range([0, width]),
+let x = d3.scaleTime().range([0, width]),
     y = d3.scaleLinear().range([height, 0]),
     z = d3.scaleOrdinal(d3.schemeCategory10);
 
-var stack = d3.stack();
+let stack = d3.stack();
 
-var area = d3.area()
+let area = d3.area()
     .x(function(d, i) { return x(d.data.date); })
     .y0(function(d) { return y(d[0]); })
     .y1(function(d) { return y(d[1]); });
 
-var g = svg.append("g")
+let g = svg.append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 d3.tsv("../output/out_normalized.tsv", type, function(error, data) {
   if (error) throw error;
 
-  var keys = data.columns.slice(1);
+  let keys = data.columns.slice(1);
 
   x.domain(d3.extent(data, function(d) { return d.date; }));
-//  z.domain(keys);
-//  stack.keys(keys);
+  z.domain(keys);
+  stack.keys(keys);
 
-  var layer = g.selectAll(".layer")
+  let layer = g.selectAll(".layer")
     .data(stack(data))
     .enter().append("g")
       .attr("class", "layer");
@@ -56,19 +56,19 @@ d3.tsv("../output/out_normalized.tsv", type, function(error, data) {
 //      .style("text-anchor", "end")
 //      .text(function(d) { return d.key; });
 
+
   layer.on('mousemove', function(d, i) {
     d3.select(this).style("fill", "red");
     console.log(i);
     console.log(d);
-    var thisName = d3.select(this).datum().key;
-    tooltip.html(thisName + " " + d.x);
+    let thisName = d3.select(this).datum().key;
+    tooltip.html(thisName + " " + d[i].data.time_stamp + " " + d[i].data[thisName]);
     tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");
   });
 
   console.log(d3.timeDay);
 
-  var xAxis = d3.axisBottom(x).ticks(d3.timeDay, "%Y-%m-%d");
-//  var xAxis = d3.axisBottom(x);
+  let xAxis = d3.axisBottom(x).ticks(d3.timeWeek.every(2)).tickFormat(d3.timeFormat("%Y-%m-%d"));
 
   g.append("g")
       .attr("class", "axis axis--x")
@@ -82,6 +82,6 @@ d3.tsv("../output/out_normalized.tsv", type, function(error, data) {
 
 function type(d, i, columns) {
   d.date = parseDate(d[columns[0]]);
-//  for (var i = 1, n = columns.length; i < n; ++i) d[columns[i]] = d[columns[i]] / 100;
+//  for (let i = 1, n = columns.length; i < n; ++i) d[columns[i]] = d[columns[i]] / 100;
   return d;
 }
